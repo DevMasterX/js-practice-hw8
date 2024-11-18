6520ce9be02ffd0e51b6f42e7df185f93117d264
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-11-18 12:18:43 +0200

    simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector('.gallery');
+const markup = createGalleryMarkup(galleryItems);
+
+galleryListEl.insertAdjacentHTML('beforeend', markup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, description, original }) => {
+      return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+        `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
